a4e3fdb9ba25e84176b85e9096e4c32b55909737
Christopher Moore <christopherkmoore1@gmail.com>
2018-08-17 14:59:44 -0700

    core data was being dumb so I took its priviledges away
    code generation was incorrectly generating optional types.

--- DividendDaily/Models/Stock.swift
+++ DividendDaily/Models/Stock.swift
@@ -10,7 +10,7 @@ import Foundation
 import CoreData
 
 class Stock: NSManagedObject {
-
+    
     override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
         super.init(entity: entity, insertInto: context)
     }
@@ -29,4 +29,50 @@ class Stock: NSManagedObject {
     }
 }
 
+extension Stock {
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Stock> {
+        return NSFetchRequest<Stock>(entityName: "Stock")
+    }
+    
+    @NSManaged public var ticker: String
+    @NSManaged public var dividend: NSOrderedSet?
+    @NSManaged public var quote: Quote?
+    
+}
+
+// MARK: Generated accessors for dividend
+extension Stock {
+    
+    @objc(insertObject:inDividendAtIndex:)
+    @NSManaged public func insertIntoDividend(_ value: Dividend, at idx: Int)
+    
+    @objc(removeObjectFromDividendAtIndex:)
+    @NSManaged public func removeFromDividend(at idx: Int)
+    
+    @objc(insertDividend:atIndexes:)
+    @NSManaged public func insertIntoDividend(_ values: [Dividend], at indexes: NSIndexSet)
+    
+    @objc(removeDividendAtIndexes:)
+    @NSManaged public func removeFromDividend(at indexes: NSIndexSet)
+    
+    @objc(replaceObjectInDividendAtIndex:withObject:)
+    @NSManaged public func replaceDividend(at idx: Int, with value: Dividend)
+    
+    @objc(replaceDividendAtIndexes:withDividend:)
+    @NSManaged public func replaceDividend(at indexes: NSIndexSet, with values: [Dividend])
+    
+    @objc(addDividendObject:)
+    @NSManaged public func addToDividend(_ value: Dividend)
+    
+    @objc(removeDividendObject:)
+    @NSManaged public func removeFromDividend(_ value: Dividend)
+    
+    @objc(addDividend:)
+    @NSManaged public func addToDividend(_ values: NSOrderedSet)
+    
+    @objc(removeDividend:)
+    @NSManaged public func removeFromDividend(_ values: NSOrderedSet)
+    
+}
 
